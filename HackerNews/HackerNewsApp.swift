0f247245cf443439ce0dcab0b32a4ee8cd2e57d2
Patryk Piwowarczyk <shepherd90@windowslive.com>
2022-02-19 15:23:00 +0100

    Initial Commit

---
+++ HackerNews/HackerNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackerNewsApp.swift
+//  HackerNews
+//
+//  Created by Patryk Piwowarczyk on 19/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct HackerNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
