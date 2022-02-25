22cc2abec4f4cde13f0775712939ef5518eed761
Patryk Piwowarczyk <shepherd90@windowslive.com>
2022-02-25 02:46:57 +0100

    Quick fix for an error when userdefaults was nil before initializing it by the user. Now app compiles fine on a physical device.

--- HackerNews/Models/NetworkManager.swift
+++ HackerNews/Models/NetworkManager.swift
@@ -18,16 +18,18 @@ class NetworkManager: ObservableObject{
     }
     
     func fetchData(){
-        //let userDefaults = UserDefaults.standard
+       let userDefaults = UserDefaults.standard
         //userDefaults.set("en", forKey: "Language")
         let base1 = "https://newsapi.org/v2/everything?q=Ukraine&from=2022-02-24&sortBy=datey"
-       let base4 = UserDefaults.standard.string(forKey: "Language")
-        //let base2 = "en"
+        let base4 = UserDefaults.standard.string(forKey: "Language")
+        let base2 = "en"
         let base3 = "45f398b987704e3c8f580ce6707170c1"
         var finalUrl: String
         if (base4 == "ua"){
             finalUrl = "https://newsapi.org/v2/top-headlines?country=ua&apiKey=45f398b987704e3c8f580ce6707170c1"
-        }else {finalUrl = "\(base1)&language=\(base4!)&apiKey=\(base3)"
+        }else if (base4 == nil) {finalUrl = "\(base1)&language=\(base2)&apiKey=\(base3)"
+        }else {
+            finalUrl = "\(base1)&language=\(base4!)&apiKey=\(base3)"
         }
        if let url = URL(string: "\(finalUrl)"){
            //if let url = URL(string: "https://newsapi.org/v2/everything?q=Ukraine&from=2022-02-24&sortBy=datey&language=pl&apiKey=45f398b987704e3c8f580ce6707170c1"){
